d9e7fef68dd89f97f1f1ad8367a4384e9ccd36ab
KerenMoreno16 <89100987+KerenMoreno16@users.noreply.github.com>
2022-05-04 20:27:31 -0300

    Starting using MySQLi Procedural

---
+++ 14-insert.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "5SI";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO estado (nomeEstado) VALUES ('Rio de Janeiro')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ 16-test_get.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+<?php
+echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
+/*Used to collect form data after submitting an HTML form */
+?>
+
+</body>
+</html>
